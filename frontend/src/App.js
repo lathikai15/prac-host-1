d4153dfa92509d992c13232860a807f23fd084a0
lathikai15 <lathikai.cse2023@citchennai.net>
2026-05-24 18:54:00 +0530

    frontend fixed finally

---
+++ frontend/src/App.js
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [data, setData] = useState([]);
+
+  // 🔹 GET data
+  const loadData = async () => {
+    const res = await axios.get("http://localhost:5000/users");
+    setData(res.data);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  // 🔹 ADD user
+  const addUser = async () => {
+    await axios.post("http://localhost:5000/users", {
+      name,
+      email,
+    });
+    setName("");
+    setEmail("");
+    loadData();
+  };
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Simple React CRUD</h2>
+
+      {/* Input */}
+      <input
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <br /><br />
+      <input
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <br /><br />
+
+      <button onClick={addUser}>Add User</button>
+
+      <hr />
+
+      {/* Display */}
+      {data.map((item) => (
+        <div key={item.id}>
+          {item.name} - {item.email}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
